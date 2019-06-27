f7dcde8da9413a1949c169033e305844782f4133
Agustin Pane <agustin.pane@gmail.com>
2019-06-27 12:44:13 -0300

    Fixs tests

--- test/delegate.test.js
+++ test/delegate.test.js
@@ -513,217 +513,74 @@ describe('## DelegateService test', () => {
         // restore stubs
         getRegisteredDelegatesStub.restore()
       })
-      it('There are 40 delegates, should return the first 35 with the best ROI', async () => {
-        const amountOfDelegates = 40
-        const roundId = 1
-        // given
-        const delegates = []
-        const transcoderRewards = []
-        const totalStakeMin = 1000
-        const totalStakeMax = 10000
-        const minReward = 100
-        let results = []
-        const amountToStake = 1000
+      it('There are 40 delegates, should return the first 25 with the best ROI', async () => {
+        //given
+        const totalDelegates = 40
         const topNumber = 25
+        const amountToStake = 1000
+        const roundId = 1
+        let resultExpected = []
+        const delegates = []
+        const rewards = []
 
-        for (let iterator = 0; iterator <= amountOfDelegates; iterator++) {
-          // Creates mock delegates
-          const newTotalStake = createTotalStake(totalStakeMin, totalStakeMax)
-          const newDelegate = createTranscoder(iterator, newTotalStake)
+        for (let iterator = 0; iterator < totalDelegates; iterator++) {
+          // Create delegates
+          const newStake = 1000
+          const stakeInTokens = unitAmountInTokenUnits(newStake)
+          const newDelegate = createTranscoder(iterator, stakeInTokens)
           delegates.push(newDelegate)
 
-          // Creates mock rewards
-          let newRewardAmount = createTotalStake(minReward, totalStakeMin) // creates a random big number between min and max
-          newRewardAmount = tokenAmountInUnits(newRewardAmount)
-          const newReward = createRewardObject(iterator, roundId, newRewardAmount)
-          transcoderRewards.push(newReward)
+          // Create rewards
+          const rewardAmount = 1000 + iterator
+          const newReward = createRewardObject(iterator, roundId, rewardAmount)
+          rewards.push(newReward)
 
-          // Creates results
-          const amountTestConvert = unitAmountInTokenUnits(amountToStake)
-          const amountRewardConvert = unitAmountInTokenUnits(newRewardAmount)
-          const newResult = delegateService.simulateNextReturnForGivenDelegatorStakedAmount(
-            amountRewardConvert,
-            newTotalStake,
-            amountTestConvert
+          // Create results
+          const rewardToDelegators = rewards[iterator].rewardTokens
+          const delegateTotalStake = delegates[iterator].totalStake
+          const delegatorAmountToStake = unitAmountInTokenUnits(amountToStake)
+          const rewardInTokens = unitAmountInTokenUnits(rewardToDelegators)
+          const newRoiResult = delegateService.simulateNextReturnForGivenDelegatorStakedAmount(
+            rewardInTokens,
+            delegateTotalStake,
+            delegatorAmountToStake
           )
-          results.push({
+          const totalStakeInUnits = tokenAmountInUnits(delegateTotalStake)
+          resultExpected.push({
             id: iterator,
-            newTotalStake,
-            roi: newResult
+            totalStake: totalStakeInUnits,
+            roi: newRoiResult
           })
         }
 
-        // Orders resultExpected desc
-        results.sort((a, b) => {
+        // Orders the results desc by ROI value
+        resultExpected.sort((a, b) => {
           const aBn = MathBN.toBig(a.roi)
           const bBn = MathBN.toBig(b.roi)
           return bBn.sub(aBn)
         })
-        // Filters the first 25 roi results
-        results = results.slice(0, topNumber)
+        // Filters the first 25 results
+        resultExpected = resultExpected.slice(0, topNumber)
 
-        // Returns mock delegates
+        // Stub registeredDelegates
         const getRegisteredDelegatesStub = sinon
           .stub(delegateService, 'getRegisteredDelegates')
           .returns(delegates)
 
-        // Returns mock rewards
+        // Stub rewards
         const getDelegateRewardToDelegatorsStub = sinon
           .stub(delegateService, 'getDelegateRewardToDelegators')
           .callsFake(delegateAddress => {
-            const rewardObject = transcoderRewards.find(element => {
-              return element.transcoder.id === delegateAddress
-            })
-            return rewardObject.rewardTokens
+            const rewardObject = rewards.find(element => element.transcoder.id === delegateAddress)
+            const rewardAmount = rewardObject.rewardTokens
+            return rewardAmount
           })
 
         // when
         const result = await delegateService.getTopDelegates(topNumber, amountToStake)
 
         // then
-        expect(result).to.deep.equal(results)
-        expect(getRegisteredDelegatesStub.called)
-        // restore stubs
-        getRegisteredDelegatesStub.restore()
-      })
-      it('There are 40 delegates, should return the first 35 with the best ROI', async () => {
-        // given
-        const delegates = []
-        const minStake = 1000
-        const minReward = 50
-        const amountToSimulateStake = 1000 // TO STAKE
-        const topNumber = 35
-        const amountOfDelegates = 40
-        let transcodersRewards = []
-        let roiResults = []
-
-        for (let iterator = 1; iterator <= amountOfDelegates; iterator++) {
-          // Create transcoders
-          const totalStake = unitAmountInTokenUnits(MathBN.mul(iterator, minStake)) // TOTAL STAKE
-          const newDelegate = createTranscoder(iterator, totalStake)
-          delegates.push(newDelegate)
-
-          // Create rewards of transcoders
-          const rewardInUnits = MathBN.mul(iterator, minReward) // REWARD
-          //console.log("amount unit", rewardInUnits)
-          const rewardAmount = unitAmountInTokenUnits(rewardInUnits)
-          transcodersRewards.push({
-            id: iterator,
-            reward: rewardAmount
-          })
-
-          const amountToSimulateStakeConverted = unitAmountInTokenUnits(amountToSimulateStake)
-          // Create results expected
-          const newResult = delegateService.simulateNextReturnForGivenDelegatorStakedAmount(
-            rewardAmount,
-            totalStake,
-            amountToSimulateStakeConverted
-          )
-          if (iterator === 1) {
-            console.log('result ts', totalStake, newResult, amountToSimulateStake)
-          }
-          roiResults.push({
-            id: iterator,
-            totalStake,
-            roi: newResult
-          })
-        }
-
-        // Orders resultExpected desc
-        roiResults.sort((a, b) => {
-          const aBn = MathBN.toBig(a.roi)
-          const bBn = MathBN.toBig(b.roi)
-          return bBn.sub(aBn)
-        })
-        // Filters the first 25 roi results
-        roiResults = roiResults.slice(0, topNumber)
-
-        const getRegisteredDelegatesStub = sinon
-          .stub(delegateService, 'getRegisteredDelegates')
-          .returns(delegates)
-
-        const getDelegateRewardToDelegatorsStub = sinon
-          .stub(delegateService, 'getDelegateRewardToDelegators')
-          .callsFake(delegateAddress => {
-            const rewardObject = transcodersRewards.find(element => element.id === delegateAddress)
-            return rewardObject.reward
-          })
-
-        // when
-        const result = await delegateService.getTopDelegates(topNumber, amountToSimulateStake)
-
-        // then
-        expect(result).to.deep.equal(roiResults)
-        expect(getRegisteredDelegatesStub.called)
-        expect(getDelegateRewardToDelegatorsStub.called)
-        // restore stubs
-        getRegisteredDelegatesStub.restore()
-        getDelegateRewardToDelegatorsStub.restore()
-      })
-      it('There are 30 delegates, should return the first 25 with the best ROI', async () => {
-        // given
-        const delegates = []
-        const minStake = 1000
-        const minReward = 50
-        const amountToSimulateStake = unitAmountInTokenUnits(1000)
-        const topNumber = 25
-        const amountOfDelegates = 30
-        let transcodersRewards = []
-        let roiResults = []
-
-        for (let iterator = 1; iterator <= amountOfDelegates; iterator++) {
-          // Create transcoders
-          const totalStakeCalc = MathBN.mul(iterator, minStake)
-          const totalStake = unitAmountInTokenUnits(totalStakeCalc)
-          const newDelegate = createTranscoder(iterator, totalStake)
-          delegates.push(newDelegate)
-
-          // Create rewards of transcoders
-          const rewardInUnits = MathBN.mul(iterator, minReward)
-          const rewardAmount = unitAmountInTokenUnits(rewardInUnits)
-          transcodersRewards.push({
-            id: iterator,
-            reward: rewardAmount
-          })
-
-          // Create results expected
-          const newResult = delegateService.simulateNextReturnForGivenDelegatorStakedAmount(
-            rewardAmount,
-            totalStake,
-            amountToSimulateStake
-          )
-          roiResults.push({
-            id: iterator,
-            totalStake: tokenAmountInUnits(totalStake),
-            roi: newResult
-          })
-        }
-
-        // Orders resultExpected desc
-        roiResults.sort((a, b) => {
-          const aBn = MathBN.toBig(a.roi)
-          const bBn = MathBN.toBig(b.roi)
-          return bBn.sub(aBn)
-        })
-        // Filters the first 25 roi results
-        roiResults = roiResults.slice(0, topNumber)
-
-        const getRegisteredDelegatesStub = sinon
-          .stub(delegateService, 'getRegisteredDelegates')
-          .returns(delegates)
-
-        const getDelegateRewardToDelegatorsStub = sinon
-          .stub(delegateService, 'getDelegateRewardToDelegators')
-          .callsFake(delegateAddress => {
-            const rewardObject = transcodersRewards.find(element => element.id === delegateAddress)
-            return rewardObject.reward
-          })
-
-        // when
-        const result = await delegateService.getTopDelegates(topNumber, amountToSimulateStake)
-
-        // then
-        expect(result).to.deep.equal(roiResults)
+        expect(result).to.deep.equal(resultExpected)
         expect(getRegisteredDelegatesStub.called)
         expect(getDelegateRewardToDelegatorsStub.called)
         // restore stubs
