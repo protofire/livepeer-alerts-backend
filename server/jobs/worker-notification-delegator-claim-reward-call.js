ea1cf93da8bd4e0493dd6f01c3656e8839870e96
Mariano Aguero <mariano.aguero@altoros.com>
2019-07-04 11:23:02 -0300

    Refactoring to a more robust worker

--- server/jobs/worker-notification-delegator-claim-reward-call.js
+++ server/jobs/worker-notification-delegator-claim-reward-call.js
@@ -1,8 +1,3 @@
-const Promise = require('bluebird')
-Promise.config({
-  cancellation: true
-})
-
 const mongoose = require('../../config/mongoose')
 const config = require('../../config/config')
 const { getProtocolService } = require('../helpers/services/protocolService')
@@ -16,78 +11,93 @@ const {
 } = require('./delegator-claim-reward-call-utils')
 
 const checkProgressRound = async () => {
+  console.log(`[Worker notification delegator claim reward call] - Start`)
+
+  const protocolService = getProtocolService()
+  let currentRoundInfo
+
   try {
-    console.log(`[Worker notification delegator claim reward call] - Start`)
-
-    const protocolService = getProtocolService()
-    const currentRoundInfo = await protocolService.getLivepeerRoundProgress()
-
-    let {
-      roundId,
-      isInitialized,
-      lastInitializedRound,
-      length,
-      startBlock,
-      progress
-    } = currentRoundInfo
-
-    let actualSavedRound = await Round.findOne({})
-    const data = {
-      roundId: roundId,
-      initialized: isInitialized,
-      lastInitializedRound: lastInitializedRound,
-      length: length,
-      startBlock: startBlock
-    }
-
-    if (!actualSavedRound) {
-      const roundCreated = new Round(data)
-      actualSavedRound = await roundCreated.save()
-    }
-
-    if (!actualSavedRound) {
-      throw new Error(`There is no actual round`)
-    }
-
-    const notificationsForRoundSent = !!actualSavedRound.notificationsForRoundSent
-    console.log(
-      `[Worker notification delegator claim reward call] - Check lock ${notificationsForRoundSent}`
-    )
-    if (progress < thresholdSendNotification && !notificationsForRoundSent) {
-      console.log(
-        `[Worker notification delegator claim reward call] - Threshold reached, sending notifications`
-      )
-
-      // Send notifications
-      await Promise.all([getSubscribersToSendEmails(), getSubscribersToSendTelegrams()])
-
-      // Save lock
-      actualSavedRound.notificationsForRoundSent = true
-      await actualSavedRound.save()
-      console.log(
-        `[Worker notification delegator claim reward call] - Lock saved ${
-          actualSavedRound.notificationsForRoundSent
-        }`
-      )
-    } else if (progress > thresholdSendNotification) {
-      // Clear lock
-      actualSavedRound.notificationsForRoundSent = false
-      await actualSavedRound.save()
-      console.log(
-        `[Worker notification delegator claim reward call] - Clear lock ${
-          actualSavedRound.notificationsForRoundSent
-        }`
-      )
-    }
-
-    console.log(
-      `[Worker notification delegator claim reward call] - The round have a progress of ${progress} %`
-    )
+    currentRoundInfo = await protocolService.getLivepeerRoundProgress()
   } catch (err) {
-    console.log(err && err.message)
+    console.error(err)
+    process.exit(1)
   }
+
+  let {
+    roundId,
+    isInitialized,
+    lastInitializedRound,
+    length,
+    startBlock,
+    progress
+  } = currentRoundInfo
+
+  let actualSavedRound = await Round.findOne({})
+  const data = {
+    roundId: roundId,
+    initialized: isInitialized,
+    lastInitializedRound: lastInitializedRound,
+    length: length,
+    startBlock: startBlock
+  }
+
+  if (!actualSavedRound) {
+    const roundCreated = new Round(data)
+    actualSavedRound = await roundCreated.save()
+  }
+
+  if (!actualSavedRound) {
+    throw new Error(`There is no actual round`)
+  }
+
+  const notificationsForRoundSent = !!actualSavedRound.notificationsForRoundSent
+
+  console.log(
+    `[Worker notification delegator claim reward call] - Check lock ${notificationsForRoundSent}`
+  )
+
+  if (progress < thresholdSendNotification && !notificationsForRoundSent) {
+    console.log(
+      `[Worker notification delegator claim reward call] - Threshold reached, sending notifications`
+    )
+
+    // Send notifications
+    try {
+      await Promise.all([getSubscribersToSendEmails(), getSubscribersToSendTelegrams()])
+    } catch (err) {
+      console.log(
+        `[Worker notification delegator claim reward call] - Error trying to send emails and telegrams`
+      )
+    }
+
+    // Save lock
+    actualSavedRound.notificationsForRoundSent = true
+    await actualSavedRound.save()
+
+    console.log(
+      `[Worker notification delegator claim reward call] - Lock saved ${actualSavedRound.notificationsForRoundSent}`
+    )
+  } else if (progress > thresholdSendNotification) {
+    // Clear lock
+    actualSavedRound.notificationsForRoundSent = false
+    await actualSavedRound.save()
+
+    console.log(
+      `[Worker notification delegator claim reward call] - Clear lock ${actualSavedRound.notificationsForRoundSent}`
+    )
+  }
+
+  console.log(
+    `[Worker notification delegator claim reward call] - The round have a progress of ${progress} %`
+  )
+
   console.log(`[Worker notification delegator claim reward call] - Finish`)
   process.exit(0)
 }
 
-return checkProgressRound()
+try {
+  return checkProgressRound()
+} catch (err) {
+  console.error(err)
+  process.exit(1)
+}
