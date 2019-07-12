840e036e8a09e37b8577fe6cda653741f599f00c
Agustin Pane <agustin.pane@gmail.com>
2019-07-12 10:07:21 -0300

    Fixs delegateSummary

--- server/helpers/services/delegateService.js
+++ server/helpers/services/delegateService.js
@@ -30,11 +30,10 @@ class DelegateService {
   getDelegateSummary = async delegateAddress => {
     const { getLivepeerDelegateAccount } = this.source
     const summary = await getLivepeerDelegateAccount(delegateAddress)
-    const { address } = summary
     return {
       summary: {
         ...summary,
-        id: address,
+        id: delegateAddress,
         totalStake: tokenAmountInUnits(_.get(summary, 'totalStake', 0))
       }
     }
