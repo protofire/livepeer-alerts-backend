1581a6937b0ab3d91c6175332330c93b89718999
Mariano Aguero <mariano.aguero@altoros.com>
2019-07-23 20:27:18 -0300

    Add sendgrid template id

--- config/config.js
+++ config/config.js
@@ -70,6 +70,11 @@ const envVarsSchema = Joi.object({
   SENDGRID_TEMPLATE_ID_NOTIFICATION_DELEGATE_CHANGE_RULES: Joi.string()
     .required()
     .description('Sendgrid template id for notification related to delegate change of rule'),
+  SENDGRID_TEMPLATE_ID_NOTIFICATION_DELEGATOR_BONDING_PERIOD_HAS_ENDED: Joi.string()
+    .required()
+    .description(
+      'Sendgrid template id for notification related to delegator bonding period has ended'
+    ),
   MAINNET_CONTROLLER_ADDRESS: Joi.string()
     .required()
     .description('Mainnet controller adddress'),
@@ -130,6 +135,8 @@ const config = {
     envVars.SENDGRID_TEMPLATE_ID_CLAIM_REWARD_UNBONDED_STATE,
   sendgridTemplateIdNotificationDelegateChangeRules:
     envVars.SENDGRID_TEMPLATE_ID_NOTIFICATION_DELEGATE_CHANGE_RULES,
+  sendgridTemplateIdNotificationDelegatorBondingPeriodHasEnded:
+    envVars.SENDGRID_TEMPLATE_ID_NOTIFICATION_DELEGATOR_BONDING_PERIOD_HAS_ENDED,
   mainnetControllerAddress: envVars.MAINNET_CONTROLLER_ADDRESS,
   rinkebyControllerAddress: envVars.RINKEBY_CONTROLLER_ADDRESS,
   thresholdSendNotification: envVars.THRESHOLD_SEND_NOTIFICATION,
